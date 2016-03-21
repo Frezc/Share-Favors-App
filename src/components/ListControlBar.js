27d4ce575e90e6a7853b2042e53a4713d14c8c0b
Frezc <504021398@qq.com>
2016-03-21 12:12:45 +0800

    完成了拖动和移动动画的兼容

--- src/components/ListControlBar.js
+++ src/components/ListControlBar.js
@@ -7,14 +7,13 @@ class ListControlBar extends React.Component {
 
     this.state = {
       showIndex: 1,
-      offsetTop: 0
+      offsetTop: 0,
+      isDragging: false,
+      showTransition: false
     };
-
-    this.isDragging = false;
   }
 
   validNumber (index, sum) {
-    console.log(index)
     sum = Math.max(1, sum);
     index = Math.min(Math.max(1, index), sum);
     return { index, sum };
@@ -49,8 +48,49 @@ class ListControlBar extends React.Component {
     return index + offsetItem;
   }
 
-  getEventHandlerClassName (isActive) {
-    if (isActive) {
+  // offsetTop -> index
+  getItemIndex (offsetTop) {
+    const { handleArea } = this.refs;
+    const { sum } = this.props;
+    
+    return Math.floor(offsetTop / handleArea.offsetHeight * (sum - 1)) + 1;
+  }
+
+  getOffsetTop (offsetY) {
+    const { offsetTop } = this.state;
+    const { handleArea } = this.refs;
+    let newOffsetTop = offsetTop + offsetY;
+
+    return Math.max(Math.min(newOffsetTop, handleArea.offsetHeight), 0);
+  }
+
+  // index -> offsetTop
+  pinToLocation (index) {
+    const { sum } = this.props;
+    const { handleArea } = this.refs;
+
+    // play transition
+    this.setState({ showTransition: true });
+
+    let offsetTop = handleArea.offsetHeight * (index - 1) / (sum - 1);
+    this.setState({
+      showIndex: index,
+      offsetTop: offsetTop,
+      isDragging: false
+    })
+  }
+
+  // fix transition bug
+  getTransition () {
+    if (this.state.showTransition) {
+      return 'top .2s';
+    } else {
+      return '';
+    }
+  }
+
+  getEventHandlerClassName () {
+    if (this.state.isDragging) {
       return 'eventHandler active';
     } else {
       return 'eventHandler inactive';
@@ -58,30 +98,32 @@ class ListControlBar extends React.Component {
   }
 
   render () {
-    let { style, description, onDragChange, onDoneChange, className, isActive } = this.props;
+    let { style, description, onDragChange, onDoneChange, className } = this.props;
 
     const { index, sum } = this.validNumber(this.state.showIndex, this.props.sum);
-
-    let zeroIndex = index - 1;
 
     return (
       <div>
         <div 
-          className={this.getEventHandlerClassName(isActive)}
+          className={this.getEventHandlerClassName()}
           onMouseMove={e => {
-            if (this.isDragging) {
+            if (this.state.isDragging) {
               console.log('onMouseMove')
-              let newIndex = this.getItemLocation(e.clientY - this.lastLocation);
+              // let newIndex = this.getItemLocation(e.clientY - this.lastLocation);
+              let offsetTop = this.getOffsetTop(e.clientY - this.lastLocation);
+              let newIndex = this.getItemIndex(offsetTop);
               onDragChange && onDragChange(newIndex);
-              // this.setState({ showIndex: newIndex });
-              this.setState({ offsetTop: this.state.offsetTop + e.clientY - this.lastLocation })
+              this.setState({ 
+                showIndex: newIndex,
+                offsetTop: offsetTop
+              });
               this.lastLocation = e.clientY;
             }
           }}
           onMouseUp={e => {
-            if (this.isDragging) {
+            if (this.state.isDragging) {
               console.log('onMouseUp')
-              this.isDragging = false;
+              this.pinToLocation(this.state.showIndex);
               onDoneChange && onDoneChange(index);
             }
           }}
@@ -95,9 +137,21 @@ class ListControlBar extends React.Component {
             <div className="slider">
               <div 
                 className="slider-top"
-                onClick={e => onDoneChange && onDoneChange(0)}
+                onClick={e => {
+                  this.pinToLocation(1);
+                  onDoneChange && onDoneChange(0);
+                }}
               >Top</div>
-              <div className="slider-bar-container">
+              <div 
+                className="slider-bar-container"
+                onClick={e => {
+                  const { offsetTop } = this.refs.container;
+                  const { offsetHeight } = this.refs.handleArea;
+                  let offsetY = e.clientY - offsetTop - 24;
+                  // 完善区间判断
+                  this.pinToLocation(this.getItemIndex(() % offsetHeight));
+                }}
+              >
                 <div 
                   className="slider-bar"
                   ref="handleArea"
@@ -108,12 +162,16 @@ class ListControlBar extends React.Component {
                   ></div>
                   <div
                     className="slider-bar-handle"
-                    style={{ top: this.state.offsetTop }}
+                    style={{ 
+                      top: this.state.offsetTop,
+                      transition: this.getTransition()
+                    }}
                     ref="handler"
                     onMouseDown={e => {
                       console.log('onMouseDown')
+                      this.setState({ showTransition: false });
                       this.lastLocation = e.clientY;
-                      this.isDragging = true;
+                      this.setState({ isDragging: true });
                     }}
                   >
                     <div className="slider-bar">
@@ -127,13 +185,16 @@ class ListControlBar extends React.Component {
                   </div>
                   <div 
                     className="slider-bar-after"
-                    style={{ height: `calc(100% - ${this.state.offsetTop + 4})` }}
+                    style={{ height: `calc(100% - ${this.state.offsetTop + 4}px)` }}
                   ></div>
                 </div>
               </div>
               <div 
                 className="slider-bottom"
-                onClick={e => onDoneChange && onDoneChange(sum)}
+                onClick={e => {
+                  this.pinToLocation(sum);
+                  onDoneChange && onDoneChange(sum);
+                }}
               >Last</div>
             </div>
           </div>
@@ -150,15 +211,13 @@ ListControlBar.propTypes = {
   sum: PropTypes.number.isRequired,
   description: PropTypes.string,
   onDragChange: PropTypes.func,
-  onDoneChange: PropTypes.func,
-  isActive: PropTypes.bool
+  onDoneChange: PropTypes.func
 };
 
 ListControlBar.defaultProps = {
   style: {},
   className: '',
-  description: '0%',
-  isActive: false
+  description: '0%'
 };
 
 const styles = {
