d8e574b7c312ed2f66b70dd38c79b6c1c2b852a0
Frezc <504021398@qq.com>
2016-04-16 12:10:27 +0800

    修改了api结构

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,13 @@
-import fetch from 'isomorphic-fetch';
 import { fetchR } from '../helpers';
-import { 
-  AUTH_URL, sendEmailUrl,
-  userInfoUrl, REGISTER_URL, refreshTokenUrl,
-  userReposUrl, userStarlistUrl
-} from './urls';
 
-// some input need to be encode
+
+// notice: fetch默认https. 这里需要注明
+// const HOST = 'http://192.168.2.236';
+const HOST = 'http://sharefavors.com';
+
+
+// 登录
+const AUTH_URL = HOST + '/auth';
 export function auth(email, password) {
   let data = new FormData();
   data.append('email', email);
@@ -19,10 +20,16 @@ export function auth(email, password) {
   });
 }
 
+
+// 发送验证邮件
+const SENDEMAIL_URL = HOST + '/sendVerifiedEmail';
 export function sendEmail(email) {
-  return fetchR(sendEmailUrl(email));
+  return fetchR(`${SENDEMAIL_URL}?email=${email}`);
 }
 
+
+// 注册
+const REGISTER_URL = HOST + '/register';
 export function register(email, password, nickname, code) {
   let data = new FormData();
   data.append('email', email);
@@ -37,18 +44,39 @@ export function register(email, password, nickname, code) {
   });
 }
 
+// 刷新token
+const REFRESHTOKEN_URL = HOST + '/refreshToken';
 export function refreshToken(token) {
-  return fetchR(refreshTokenUrl(token));
+  return fetchR(`${REFRESHTOKEN_URL}?token=${token}`);
 }
 
-export function userInfo(id) {
-  return fetchR(userInfoUrl(id));
+// 得到用户信息
+export function userInfo(id, starListMax = 3, repositoriesMax = 3) {
+  return fetchR(`${HOST}/user/${id}?starListMax=${starListMax}&repositoriesMax=${repositoriesMax}`);
 }
 
+
+// 得到用户的仓库列表
 export function userRepository(id, orderby = 'recent updated', offset = 0, token, limit = 50) {
-  return fetchR(userReposUrl(id, offset, limit, orderby, token))
+  const tokenParam = token ? `&token=${token}` : '';
+
+  return fetchR(`${HOST}/user/${id}/repository?offset=${offset}&orderby=${orderby.toLowerCase()}&limit=${limit}${tokenParam}`)
 }
 
+// 得到用户的starList
 export function userStarlist(id, offset = 0, token, limit = 50) {
-  return fetchR(userStarlistUrl(id, offset, limit, token));  
+  const tokenParam = token ? `&token=${token}` : '';
+
+  return fetchR(`${HOST}/user/${id}/starList?offset=${offset}&limit=${limit}${tokenParam}`);  
 }
+
+
+// 重置密码
+const RESETPASSWORD_URL = HOST + '/resetPassword';
+
+// 取消授权
+const UNAUTH_URL = HOST + '/unauth';
+export function unauthUrl(token) {
+  return `${UNAUTH_URL}?token=${token}`
+}
+
